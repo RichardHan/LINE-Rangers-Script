fa83f7c9ae8bd671977c2dab1974a29b49f8496c
RichardHan <qwweee7467@gmail.com>
2016-01-20 21:42:38 +0800

    修正出現寶物卡住問題
    座標重新抓取

--- adb/b.bat
+++ adb/b.bat
@@ -1,10 +1,10 @@
 echo on
 
 
-adb connect localhost
-adb devices 
-adb forward tcp:7000 tcp:7000
-adb forward tcp:7001 tcp:7001
+.\adb\adb connect localhost
+.\adb\adb devices 
+.\adb\adb forward tcp:7000 tcp:7000
+.\adb\adb forward tcp:7001 tcp:7001
 
 
 pause
