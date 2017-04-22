4906577534f4d48c3e412e4148a37ff52283b040
RichardHan <qwweee7467@gmail.com>
2017-04-22 10:05:16 +0800

    add v301 , fix 選朋友畫素問題

--- adb/b.bat
+++ adb/b.bat
@@ -1,8 +1,9 @@
 echo on
 
-.\adb\adb connect localhost
+.\adb\adb connect 127.0.0.1:26944:5037
 .\adb\adb devices 
 .\adb\adb forward tcp:7000 tcp:7000
 .\adb\adb forward tcp:7001 tcp:7001
 
+pause
 exit
